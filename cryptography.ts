df64da9ebdd0cd1e89e5ebbe173075a051bc5afb
Bunny Bites <s.mukeshkr481@gmail.com>
2024-02-28 11:55:36 +0530

    add cryptography demo using typescript

--- cryptography.ts
+++ cryptography.ts
@@ -1,6 +1,15 @@
 import { Keypair } from '@solana/web3.js';
+import dotenv from 'dotenv';
+import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 
-const keypair = Keypair.generate();
+// const keypair = Keypair.generate();
 
-console.log("public key", keypair.publicKey.toBase58());
-console.log("secret key", keypair.secretKey);
+dotenv.config();
+
+const printkeypairDetails = (providedKeypair: Keypair) => {
+    console.log("public key", providedKeypair.publicKey.toBase58());
+    console.log("secret key", providedKeypair.secretKey);
+}
+
+let keypair  = getKeypairFromEnvironment("SECRET_KEY");
+printkeypairDetails(keypair);
