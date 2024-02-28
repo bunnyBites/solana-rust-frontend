a4a7fe5a2df5d3ac42b4f5024eac6c2d695aa015
Bunny Bites <s.mukeshkr481@gmail.com>
2024-02-28 11:57:23 +0530

    reduce LOC

--- cryptography.ts
+++ cryptography.ts
@@ -1,10 +1,8 @@
 import { Keypair } from '@solana/web3.js';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import { getKeypairFromEnvironment } from '@solana-developers/helpers';
 
 // const keypair = Keypair.generate();
-
-dotenv.config();
 
 const printkeypairDetails = (providedKeypair: Keypair) => {
     console.log("public key", providedKeypair.publicKey.toBase58());
