73263c32671c48957c4d5c29b9caf7329fe08ebb
Bunny Bites <s.mukeshkr481@gmail.com>
2024-02-29 17:20:15 +0530

    add transaction example

--- main.ts
+++ main.ts
@@ -4,8 +4,8 @@ import readSolanaExampleScript from './module_one/readSolanaBlockchain';
 import transactionExampleFn from './module_one/transaction';
 
 const main = () => {
-    cryptographyExampleScript();
-    readSolanaExampleScript();
+    // cryptographyExampleScript();
+    // readSolanaExampleScript();
     transactionExampleFn();
 }
 
--- module_one/transaction.ts
+++ module_one/transaction.ts
@@ -1,5 +1,29 @@
-const transactionExampleFn = () => {
-    console.log('hi');
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import {
+    Connection, LAMPORTS_PER_SOL, PublicKey,
+    SystemProgram, Transaction, clusterApiUrl,
+    sendAndConfirmTransaction,
+} from "@solana/web3.js";
+
+const transactionExampleFn = async () => {
+    const transaction = new Transaction();
+
+    const transferInstruction = SystemProgram.transfer({
+        fromPubkey: new PublicKey(process.env.MY_PUBLIC_KEY ?? ""),
+        toPubkey: new PublicKey(process.env.FRIEND_PUBLIC_KEY ?? ""),
+        lamports: 1 * LAMPORTS_PER_SOL,
+    });
+
+    transaction.add(transferInstruction);
+
+    const connection = new Connection(clusterApiUrl("devnet"));
+    const signer = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [getKeypairFromEnvironment("SECRET_KEY")],
+    );
+
+    console.log("Transaction completed!!", signer);
 }
 
 export default transactionExampleFn;
