27a375dcc9f5b07237763118972aef6dcd88f3de
Bunny Bites <s.mukeshkr481@gmail.com>
2024-02-29 16:57:41 +0530

    add common file to run example script

---
+++ main.ts
@@ -0,0 +1,11 @@
+import cryptographyExampleScript from './cryptography';
+import readSolanaExampleScript from './readSolanaBlockchain';
+import transactionExampleFn from './transaction';
+
+const main = () => {
+    cryptographyExampleScript();
+    readSolanaExampleScript();
+    transactionExampleFn();
+}
+
+main();
