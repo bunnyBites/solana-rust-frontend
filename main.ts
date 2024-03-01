e02f2d04587cb1935322628ede1aa53af717a2d8
Bunny Bites <s.mukeshkr481@gmail.com>
2024-03-01 18:33:48 +0530

    format file

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
-import 'dotenv/config';
-import cryptographyExampleScript from './module_one/cryptography';
-import readSolanaExampleScript from './module_one/readSolanaBlockchain';
-import { transactionExampleFn, runCustomOnChainTransactionExample } from './module_one/transaction';
+import "dotenv/config";
+import cryptographyExampleScript from "./module_one/cryptography";
+import readSolanaExampleScript from "./module_one/readSolanaBlockchain";
+import {
+  transactionExampleFn,
+  runCustomOnChainTransactionExample,
+} from "./module_one/transaction";
 
 const main = () => {
-    // cryptographyExampleScript();
-    // readSolanaExampleScript();
-    // transactionExampleFn();
-    runCustomOnChainTransactionExample();
-}
+  // cryptographyExampleScript();
+  // readSolanaExampleScript();
+  // transactionExampleFn();
+  runCustomOnChainTransactionExample();
+};
 
 main();
--- module_one/readSolanaBlockchain.ts
+++ module_one/readSolanaBlockchain.ts
@@ -1,13 +1,18 @@
-import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl,
+} from "@solana/web3.js";
 
 const runReadSolanaExample = async () => {
-    let connection = new Connection(clusterApiUrl("devnet"));
+  let connection = new Connection(clusterApiUrl("devnet"));
 
-    // most addresses are public key in solana network
-    let address = new PublicKey("BSSCXA2vyVNPCLZeGtMyR5N4y98eMhqifsVdBJkaCKXL");
+  // most addresses are public key in solana network
+  let address = new PublicKey("BSSCXA2vyVNPCLZeGtMyR5N4y98eMhqifsVdBJkaCKXL");
 
-    let balance = await connection.getBalance(address);
-    console.log("Balance for the account is:", balance / LAMPORTS_PER_SOL, "SOL");
-}
+  let balance = await connection.getBalance(address);
+  console.log("Balance for the account is:", balance / LAMPORTS_PER_SOL, "SOL");
+};
 
 export default runReadSolanaExample;
