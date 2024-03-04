e474be4e64afc14e5fe755bdac0635969f6089a0
Bunny Bites <s.mukeshkr481@gmail.com>
2024-03-04 21:06:00 +0530

    remove unused imports

--- main.ts
+++ main.ts
@@ -1,10 +1,5 @@
 import "dotenv/config";
-import cryptographyExampleScript from "./module_one/cryptography";
-import readSolanaExampleScript from "./module_one/readSolanaBlockchain";
-import {
-  transactionExampleFn,
-  runCustomOnChainTransactionExample,
-} from "./module_one/transaction";
+import { runCustomOnChainTransactionExample } from "./module_one/transaction";
 
 const main = () => {
   // cryptographyExampleScript();
