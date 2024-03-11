3f46a802d0e5896666490f2f654489f36bf48f55
Bunny Bites <s.mukeshkr481@gmail.com>
2024-03-11 11:59:21 +0530

    add minor static string change

--- module_one/cryptography.ts
+++ module_one/cryptography.ts
@@ -5,8 +5,8 @@ const runCryptographyFn = () => {
   // const keypair = Keypair.generate();
 
   const printkeypairDetails = (providedKeypair: Keypair) => {
-    console.log("public key", providedKeypair.publicKey.toBase58());
-    console.log("secret key", providedKeypair.secretKey);
+    console.log("public key: ", providedKeypair.publicKey.toBase58());
+    console.log("secret key: ", providedKeypair.secretKey);
   };
 
   let keypair = getKeypairFromEnvironment("SECRET_KEY");
