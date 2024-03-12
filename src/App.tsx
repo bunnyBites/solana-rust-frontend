add3ed120ec62600cf0560cdb6ced76bd03073f7
Bunny Bites <s.mukeshkr481@gmail.com>
2024-03-12 22:10:37 +0530

    add code to test wallet adapter (react-ui)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
-const App = () => <h3>hello</h3>
+import { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { Home } from "./screen/home/Home";
+
+const App = () => {
+  const endpoint = clusterApiUrl("devnet");
+
+  const wallets = useMemo(() => [], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <div>
+            <Home />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
 
 export default App;
